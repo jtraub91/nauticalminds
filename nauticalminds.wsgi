96fc667b7a18a919bbe1bb9758574f50aa3d6af5
Jason Traub <jtraub91@gmail.com>
2024-07-02 00:26:46 -0700

    remove unnecessary / duplicate files, modify + remove web3 to fix build, other edits

--- nauticalminds.wsgi
+++
@@ -1,1 +0,0 @@
-from nauticalminds import app as application
--- src/NauticalMinds.jsx
+++ src/NauticalMinds.jsx
@@ -1,12 +1,8 @@
-import Web3 from "web3";
-
 import React, { useState, useEffect } from "react";
 
 import AudioBar from "./components/AudioBar.jsx";
 import AboutModal from "./components/AboutModal.jsx";
-import ConnectModal from "./components/ConnectModal.jsx";
 import Header from "./components/Header.jsx";
-import { getCookieValue, clearCookie, parseJwt } from "./utils";
 import TipModal from "./components/TipModal.jsx";
 
 import config from "./config";
@@ -25,40 +21,6 @@ function NauticalMinds(props) {
   const [metaData, setMetaData] = useState({});
 
   useEffect(() => {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-
-      window.web3.eth
-        .getAccounts()
-        .then((accounts) => {
-          let token = getCookieValue("token");
-          if (accounts.length > 0 && token) {
-            let payload = parseJwt(token);
-            let acct = accounts[0].toLowerCase();
-            if (acct !== "0x" + payload.ethAddress) {
-              console.log("invalid token cookie will be cleared");
-              clearCookie("token");
-            } else {
-              console.log("valid cookie");
-              setUserAccount("0x" + payload.ethAddress.toUpperCase()); // capitalize
-              fetchMetadata();
-            }
-          } else if (accounts.length > 0) {
-            console.log("connected accounts but no jwt token");
-            console.log(accounts);
-          } else {
-            console.log("no connected accounts");
-          }
-        })
-        .catch((e) => console.error(e));
-      window.ethereum.on("accountsChanged", (accounts) => {
-        clearCookie("token");
-        setUserAccount(""); // force user to click connect and sign in
-      });
-      window.ethereum.on("chainChanged", (chainId) => window.location.reload());
-    } else {
-      console.log("Please install MetaMask");
-    }
     fetchMetadata();
   }, []);
   function fetchMetadata() {
@@ -94,61 +56,6 @@ function NauticalMinds(props) {
     setMintModalVisible(false);
     setTipModalVisible(false);
   }
-  function connectMetamaskCallback(accounts) {
-    let acct = accounts[0].toLowerCase();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function (resp) {
-      if (xhr.readyState === 4) {
-        let data = resp.currentTarget.response;
-        data = JSON.parse(data);
-        if ("0x" + data.ethAddress == acct && data.sigRequest !== undefined) {
-          window.web3.eth.personal
-            .sign(data.sigRequest, acct)
-            .then((sig) => {
-              let req = new XMLHttpRequest();
-              req.onreadystatechange = (resp) => {
-                if (req.readyState === 4) {
-                  let sigResp = JSON.parse(resp.currentTarget.responseText);
-                  if (sigResp.error) {
-                    console.error(sigResp.error);
-                  } else {
-                    // set token cookie
-                    document.cookie = `token=${sigResp.token}; Path=/; SameSite=strict;`; // todo: expires
-                    setConnectModalVisible(false);
-                    setUserAccount(data.ethAddress.toUpperCase());
-                    fetchMetadata();
-                  }
-                }
-              };
-              req.onerror = (e) => console.error(e);
-              req.open("POST", "/sig");
-              req.setRequestHeader("Content-Type", "application/json");
-              req.send(
-                JSON.stringify({
-                  ethAddress: data.ethAddress,
-                  signature: sig,
-                })
-              );
-            })
-            .catch((e) => {
-              console.error(e);
-            });
-        } else {
-          console.debug(resp);
-        }
-      }
-    };
-    xhr.onerror = function (err) {
-      console.error(err);
-    };
-    xhr.open("POST", "/connect");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(
-      JSON.stringify({
-        ethAddress: acct,
-      })
-    );
-  }
   return (
     <div>
       <Header
@@ -163,11 +70,6 @@ function NauticalMinds(props) {
         userAccount={userAccount}
         visible={mintModalVisible}
         onClick={modalBackdropOnClick}/> */}
-      <ConnectModal
-        connectMetamaskCallback={connectMetamaskCallback}
-        visible={connectModalVisible}
-        onClick={modalBackdropOnClick}
-      />
       <AboutModal visible={aboutModalVisible} onClick={modalBackdropOnClick} />
       <TipModal
         btcWalletAddress={config.btcWalletAddress}
